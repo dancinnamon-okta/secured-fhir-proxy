e598aa8ede81b93616076eb0f25baba7f461f6c6
Dan Cinnamon <42472957+dancinnamon-okta@users.noreply.github.com>
2024-04-22 10:49:06 -0500

    Minor updates from testing with different clients.

--- aws/read_operation.js
+++ aws/read_operation.js
@@ -3,6 +3,8 @@ const readLib = require('../lib/read_operation')
 
 module.exports.readHandler = async (event, context) => {
 	const readResult = await readLib.readHandler(event.requestContext.authorizer, event.pathParameters.tenantId, event.path, event.queryStringParameters, event.headers)
+	readResult.headers['Access-Control-Allow-Origin'] = '*'
+	readResult.headers['Access-Control-Allow-Credentials'] = 'true'
 	return {
 		statusCode: readResult.statusCode,
 		body: JSON.stringify(readResult.body),
